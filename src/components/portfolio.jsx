26dde34b466b4a3b746c8e982461b6eb4df45bcd
Sofi.Maye <sofi.mann99@gmail.com>
2022-06-04 13:57:05 +0300

    fixed library vs deleted old

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -3,51 +3,22 @@ import React, {Component} from 'react';
 import 'photoswipe/dist/photoswipe.css'
 import { Gallery, Item } from 'react-photoswipe-gallery'
 
-const MyGallery = () => (
-    <Gallery>
-        <Item
-            original="images/IMGnewsize.jpg"
-            thumbnail="images/IMGnewsize.jpg"
-        >
-            {({ ref, open }) => (
-                <img ref={ref} onClick={open} src="/images/IMGnewsize.jpg" />
-            )}
-        </Item>
-        <Item
-            original="images/IMGnewsize16.jpg"
-            thumbnail="images/IMGnewsize16.jpg"
-        >
-            {({ ref, open }) => (
-                <img ref={ref} onClick={open} src="/images/IMGnewsize16.jpg" />
-            )}
-        </Item>
-    </Gallery>
-)
-
-class GridItem extends Component {
-    render() {
-        return(
-            <div id="grid-item" className="grid-item">
-                <a data-fslightbox='gallery'>
-                    <img alt="image" src={this.props.src}/>
-                </a>
-            </div>
-        )
-    }
-}
-
-
 const images = ["images/IMGnewsize.jpg", "images/IMGnewsize16.jpg",
     "images/IMGnewsize2.jpg", "images/IMGnewsize3.jpg", "images/IMGnewsize4.jpg",
     "images/IMGnewsize5.jpg", "images/IMGnewsize6.jpg", "images/IMGnewsize7.jpg",
     "images/IMGnewsize8.jpg"]
 
 export class Portfolio extends Component {
-    render(){
-        return (
+    render() {
+        return(
             <div id="grid" className="grid">
-                {/*{images.map((image, i) => (<GridItem src={image} key={i}/>))}*/}
-                <MyGallery/>
+                <Gallery>
+                {images.map((image, i) => (<div id="grid-item" className="grid-item">
+                    <Item width="724" height="1024" original={image} thumbnail={image} key={i}>
+                    {({ ref, open }) => (
+                        <img alt="image" ref={ref} onClick={open} src={image} />
+                    )}</Item></div>))}
+                </Gallery>
             </div>
         )
     }
